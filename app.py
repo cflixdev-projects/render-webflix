d407086a84c14783b23f6f709a38e3e9d34b4d01
Mert Polat <mertlet04@gmail.com>
2024-03-14 21:47:07 +0100

    idk how that works but it works

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ options.add_argument('log-level=3')
 driver = webdriver.Chrome(options=options)
 
 
+def get_new_link_from_redirect(redirect_url):
+    try:
+        driver.get(redirect_url)
+        new_link = driver.current_url
+        return new_link
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+
+
 def get_video_link(show_name, season, episode):
     try:
         link = f"http://186.2.175.5/serie/stream/{show_name}/staffel-{season}/episode-{episode}"
@@ -25,16 +35,31 @@ def get_video_link(show_name, season, episode):
         print("An error occurred:", e)
         return None
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/search', methods=['POST'])
 def search():
     text_input = request.form['textInput'].replace(' ', '')
     show_name, season, episode = text_input.split(',')
-    video_link = get_video_link(show_name, season, episode)
-    return video_link
+    video_link = None
+
+    try:
+        redirect_url = text_input  # Assuming text_input contains the redirect URL
+        new_url = get_new_link_from_redirect(redirect_url)
+
+        if new_url:
+            video_link = new_url
+        else:
+            video_link = get_video_link(show_name, season, episode)
+    except Exception as e:
+        print("An error occurred:", e)
+
+    return video_link or "Error: Video link not found"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
