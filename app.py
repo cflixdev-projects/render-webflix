327308dec6b7e2b4a9f109b7780399b0b3b870ab
Mert Polat <mertlet04@gmail.com>
2024-03-15 11:25:18 +0100

    loads faster

--- app.py
+++ app.py
@@ -5,21 +5,19 @@ from selenium import webdriver
 
 app = Flask(__name__)
 
-# Initialize the WebDriver instance outside of the function
+# Initialize the WebDriver instance outside of the Flask application context
 options = Options()
 options.add_argument("--headless")
 options.add_argument('log-level=3')
 driver = webdriver.Chrome(options=options)
 
-
-def get_new_link_from_redirect(redirect_url):
+def get_new_link_from_redirect(driver, redirect_url):
     driver.get(redirect_url)
     new_link = driver.current_url
     print('aktueller neuere link ' + new_link)
     return new_link
 
-
-def get_video_link(show_name, season, episode):
+def get_video_link(driver, show_name, season, episode):
     link = f"http://186.2.175.5/serie/stream/{show_name}/staffel-{season}/episode-{episode}"
     driver.get(link)
     element = driver.find_element(By.CSS_SELECTOR,
@@ -33,14 +31,13 @@ def get_video_link(show_name, season, episode):
 def index():
     return render_template('index.html')
 
-
 @app.route('/search', methods=['POST'])
 def search():
     text_input = request.form['textInput'].replace(' ', '')
     show_name, season, episode = text_input.split(',')
 
-    redirect_url = get_video_link(show_name, season, episode)
-    new_url = get_new_link_from_redirect(redirect_url)
+    redirect_url = get_video_link(driver, show_name, season, episode)
+    new_url = get_new_link_from_redirect(driver, redirect_url)
     return new_url
 
 if __name__ == '__main__':
