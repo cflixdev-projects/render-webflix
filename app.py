5b94351da2ecf9782be29c9e09b33783153db6ef
cflixdev-projects <cflixdev@proton.me>
2024-07-28 11:03:55 +0200

    mit filmen

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 app = Flask(__name__)
-
 
 # Initialize the WebDriver instance outside of the Flask application context
 options = Options()
@@ -28,18 +29,38 @@ def get_video_link(driver, show_name, season, episode):
     print('content_value = ' + content_value)
     return content_value
 
+def get_movie_link(driver, show_name):
+    link = f"https://cinemathek.net/filme/{show_name}"
+    driver.get(link)
+    iframe_element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe.metaframe'))
+    )
+
+    # Extract the src attribute from the iframe element
+    src_url = iframe_element.get_attribute('src')
+    print(f'The src URL of the iframe is: {src_url}')
+    return src_url
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
 def search():
+    switch_value = request.form['switchValue']  # Retrieve switchValue from the form data
     text_input = request.form['textInput'].replace(' ', '')
-    show_name, season, episode = text_input.split(',')
 
-    redirect_url = get_video_link(driver, show_name, season, episode)
-    new_url = get_new_link_from_redirect(driver, redirect_url)
-    return new_url
+    if switch_value == 'Shows':
+        show_name, season, episode = text_input.split(',')
+        redirect_url = get_video_link(driver, show_name, season, episode)
+        new_url = get_new_link_from_redirect(driver, redirect_url)
+        return new_url
+    elif switch_value == 'Movies':
+        show_name = text_input.strip()
+        print(show_name)
+        redirect_url = get_movie_link(driver, show_name)
+        new_url = get_new_link_from_redirect(driver, redirect_url)
+        return new_url
 
 if __name__ == '__main__':
     app.run(debug=True)
