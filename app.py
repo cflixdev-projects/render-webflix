17716614d7c9e0b11b5da16a6ed86778d87bea2e
cflixdev-projects <cflixdev@proton.me>
2024-07-29 16:34:13 +0200

    nur filme

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 app = Flask(__name__)
 
@@ -18,7 +18,6 @@ def create_driver():
     driver = webdriver.Chrome(options=options)
     return driver
 
-# Initialize the WebDriver instance outside of the Flask application context
 
 def get_new_link_from_redirect(redirect_url):
     driver = create_driver()
@@ -31,19 +30,6 @@ def get_new_link_from_redirect(redirect_url):
         driver.quit()
     return new_link
 
-
-def get_show_link(show_name, season, episode):
-    driver = create_driver()
-    try:
-        link = f"http://186.2.175.5/serie/stream/{show_name}/staffel-{season}/episode-{episode}"
-        driver.get(link)
-        element = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '#wrapper > div.seriesContentBox > div.container.marginBottom > div:nth-child('
-                                      '5) > div.hosterSiteVideo > div.inSiteWebStream > div:nth-child(1) > iframe')))
-        content_value = element.get_attribute('src')
-        return content_value
-    except Exception as e:
-        return None
 
 def get_video_link(show_name):
     driver = create_driver()
@@ -64,49 +50,19 @@ def get_video_link(show_name):
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
 def search():
-        # switch_value = request.form['switchValue']
-        text_input = request.form['textInput'].replace(' ', '')
-        show_name = text_input.strip()
+    text_input = request.form['textInput'].replace(' ', '')
+    show_name = text_input.strip()
 
-        # if switch_value == 'Movies':
-        redirect_url = get_video_link(show_name)
-        if redirect_url:
-            new_url = get_new_link_from_redirect(redirect_url)
-            return new_url
-        else:
-            return "Video Link not found"
-
-
-        '''if switch_value == 'Shows':
-            try:
-                text_input = request.form['textInput'].replace(' ', '')
-                show_name, season, episode = text_input.split(',')
-                redirect_url = get_show_link(show_name, season, episode)
-            except ValueError:
-                return jsonify({"error": "Invalid input format for shows. Expected format: show_name,season,episode"}), 400
-        elif switch_value == 'Movies':
-            show_name = text_input.strip()
-            redirect_url = get_video_link(show_name)
-        else:
-            return jsonify({"error": "Invalid switch value"}), 400
-
-        if redirect_url:
-            new_url = get_new_link_from_redirect(redirect_url)
-            if new_url:
-                return new_url
-            else:
-                return jsonify({"error": "Failed to retrieve new URL from redirect"}), 500
-        else:
-            return jsonify({"error": "Failed to retrieve redirect URL"}), 500
-    except Exception as e:
-        return jsonify({"error": "Internal server error"}), 500
-'''
+    redirect_url = get_video_link(show_name)
+    if redirect_url:
+        new_url = get_new_link_from_redirect(redirect_url)
+        return new_url
+    else:
+        return "Video link not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
-
