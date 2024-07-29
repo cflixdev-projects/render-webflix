107a71fea039c9ce8f66661719d67665ae55fa4b
cflixdev-projects <cflixdev@proton.me>
2024-07-29 17:19:19 +0200

    nur filme

--- app.py
+++ app.py
@@ -1,31 +1,52 @@
 from flask import Flask, render_template, request
+from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 app = Flask(__name__)
 
-# Initialize the WebDriver instance outside of the Flask application context
-options = Options()
-options.add_argument("--headless")
-options.add_argument('log-level=3')
-driver = webdriver.Chrome(options=options)
+def create_driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('log-level=3')
+    driver = webdriver.Chrome(options=options)
+    return driver
 
-def get_new_link_from_redirect(driver, redirect_url):
-    driver.get(redirect_url)
-    new_link = driver.current_url
-    print('aktueller neuere link ' + new_link)
+
+def get_new_link_from_redirect(redirect_url):
+    driver = create_driver()
+    try:
+        driver.get(redirect_url)
+        new_link = driver.current_url
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        driver.quit()
     return new_link
 
-def get_video_link(driver, show_name, season, episode):
-    link = f"http://186.2.175.5/serie/stream/{show_name}/staffel-{season}/episode-{episode}"
-    driver.get(link)
-    element = driver.find_element(By.CSS_SELECTOR,
-                                  '#wrapper > div.seriesContentBox > div.container.marginBottom > div:nth-child('
-                                  '5) > div.hosterSiteVideo > div.inSiteWebStream > div:nth-child(1) > iframe')
-    content_value = element.get_attribute('src')
-    print('content_value = ' + content_value)
-    return content_value
+
+def get_video_link(show_name):
+    driver = create_driver()
+    try:
+        link = f"https://cinemathek.net/filme/{show_name}"
+        driver.get(link)
+        try:
+            iframe_element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe.metaframe'))
+            )
+            src_url = iframe_element.get_attribute('src')
+        except TimeoutException:
+            print("Das iframe-Element konnte nicht innerhalb von 20 Sekunden gefunden werden.")
+            src_url = None
+    finally:
+        driver.quit()
+    return src_url
 
 @app.route('/')
 def index():
@@ -34,11 +55,14 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     text_input = request.form['textInput'].replace(' ', '')
-    show_name, season, episode = text_input.split(',')
+    show_name = text_input.strip()
 
-    redirect_url = get_video_link(driver, show_name, season, episode)
-    new_url = get_new_link_from_redirect(driver, redirect_url)
-    return new_url
+    redirect_url = get_video_link(show_name)
+    if redirect_url:
+        new_url = get_new_link_from_redirect(redirect_url)
+        return new_url
+    else:
+        return "Video link not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
